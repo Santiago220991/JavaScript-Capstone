a555a86f78f5cfe6457c49930ddd54d3b660d55e
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-05-16 16:33:13 -0500

    Add feature to display items form the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles.css";
+import * as api from "./modules/api-functions.js"
+
+const section=document.querySelector(".moviediv")
+
+const apiurl="https://api.tvmaze.com/search/shows?q=comedy"
+
+let imgurl
+const myPromise = new Promise((resolve) => {
+    resolve(api.getdata(apiurl))
+});
+
+myPromise.then(result => { 
+    imgurl=api.getimg(result)
+    console.log(imgurl)
+    imgurl.forEach((element,index)=>{
+        section.innerHTML+=`<div class="item">
+        <img class="poster" src="${element}" alt="Italian Trulli">
+        <div class="title"><p>${result[index].name}</p>
+        <div>
+        <img class="like" src="./peace_img.png">
+        <p>5 likes</p>
+        </div>
+        </div>
+        <p class="comments">Comments</p>
+        <p class="reservations">Reservations</p>  
+        </div>`
+    })
+
+})
+
+
+
