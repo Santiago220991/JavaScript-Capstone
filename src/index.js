d5101b74ce65761278cbe64fe0658e901eb8eff2
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-05-16 17:44:13 -0500

    Check linters, add styles to home page

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-import "./styles.css";
-import * as api from "./modules/api-functions.js"
+import './styles.css';
+import * as api from './modules/api-functions.js';
+import likeimg from './like_img.png';
+import moviesimg from './movies_img.png';
 
-const section=document.querySelector(".moviediv")
+const section = document.querySelector('.moviediv');
+const logo = document.querySelector('header img');
+const apiurl = 'https://api.tvmaze.com/search/shows?q=comedy';
+let imgurl;
 
-const apiurl="https://api.tvmaze.com/search/shows?q=comedy"
+logo.src = moviesimg;
 
-let imgurl
 const myPromise = new Promise((resolve) => {
-    resolve(api.getdata(apiurl))
+  resolve(api.getdata(apiurl));
 });
 
-myPromise.then(result => { 
-    imgurl=api.getimg(result)
-    console.log(imgurl)
-    imgurl.forEach((element,index)=>{
-        section.innerHTML+=`<div class="item">
+myPromise.then((result) => {
+  imgurl = api.getimg(result);
+  imgurl.forEach((element, index) => {
+    section.innerHTML += `<div class="item">
         <img class="poster" src="${element}" alt="Italian Trulli">
         <div class="title"><p>${result[index].name}</p>
         <div>
-        <img class="like" src="./peace_img.png">
-        <p>5 likes</p>
+        <img class="like" src="${likeimg}">
+        <p class="like-text">5 likes</p>
         </div>
         </div>
         <p class="comments">Comments</p>
         <p class="reservations">Reservations</p>  
-        </div>`
-    })
-
-})
-
-
-
+        </div>`;
+  });
+});
